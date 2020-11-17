f97e61c6702f8a754caa26710e5de2cec6eb5bf0
Scott Wichser <blast007@users.sourceforge.net>
2020-11-16 21:48:56 -0600

    Detect HTTPS use on a reverse proxy and adjust URI.

--- config.sample.php
+++ config.sample.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'protocol' => !empty($_SERVER['HTTPS'] ?? '')? 'https' : 'http',
+    'protocol' => (!empty($_SERVER['HTTPS'] ?? '') || ($_SERVER['HTTP_X_FORWARDED_PROTO'] ?? '') == 'https')? 'https' : 'http',
     'hostname' => 'my.bzflag.org',
-    'baseURI' => '/listkeymgr/',
+    'baseURI' => '/listkeys/',
     'checkip' => true // Set to false for development
 ];
